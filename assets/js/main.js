72788bc71e822c467747e11482f40b8a646381db
jnleyva816 <32456470+jnleyva816@users.noreply.github.com>
2021-06-12 09:45:35 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*========== MENU SHOW Y HIDDEN ==========*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/*======= MENU SHOW =======*/
+/*validate if constant exists*/
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*======= MENU HIDDEN =======*/
+/*validate if constant exists*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*========== REMOVE MENU MOBILE ==========*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementsById('nav-menu')
+    // When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*========== ACCORDIAN SKILLS ==========*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+    
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
+/*========== QUALIFICATION TABS ==========*/
+const tabs = document.querySelectorAll('[data-target]')
+       tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+/*========== SERVICE MODEL ==========*/
+
+/*========== PORTFOLIO SWIPER ==========*/
+
+/*========== TESTIMONIAL ==========*/
+
+/*========== SCROLL SECTION ACTIVE LINK ==========*/
+
+/*========== QUALIFICATION TABS ==========*/
